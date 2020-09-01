af37d83b5d0d97006ea8bebafd46ab40e0e95942
DiemBTran <diembtran@gmail.com>
2020-09-01 13:49:46 -0700

    Second commit - added string and boolean

--- javascript-introduction-diem-drew.js
+++ javascript-introduction-diem-drew.js
@@ -1,6 +1,25 @@
+// Arithmetic
+console.log("Arthimetic answers");
+
 console.log(34 + 71);
 console.log(123 - 67);
 console.log(56 * 23);
 console.log(45 / 5);
 console.log(5 ** 7);
 console.log(33 % 6);
+
+// String
+console.log("String answers");
+var name = "Diem";
+
+console.log(name.length);
+console.log(name.includes("e"));
+
+// Boolean
+console.log("Boolean answers");
+console.log(34/3 > 67/2);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(!3 === 3);
+console.log("LEARN".length === 5 && "Student".length === 7);
+console.log("LEARN".length === 5 || "Student".length === 10);
