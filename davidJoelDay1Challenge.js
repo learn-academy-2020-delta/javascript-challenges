4d73d190f9a740d4ed68f4d5d846b06072f5c86f
davidkim4 <dkim4016@gmail.com>
2020-09-01 16:17:14 -0700

    last var

--- davidJoelDay1Challenge.js
+++ davidJoelDay1Challenge.js
@@ -8,9 +8,11 @@
 //console.log(5 ** 7);
 //console.log(33 % 6);
 
+
 //Strings
 //console.log("David".length);
 //console.log("Joel".includes("e"));
+
 
 //Booleans
 //console.log(34 / 3 > 67 / 2);
@@ -19,6 +21,7 @@
 //console.log(!3 === 3);
 //console.log("LEARN".length === 5 && "student".length === 7);
 //console.log("LEARN".length === 5 || "student".length === 10);
+
 
 //Variables
 //var favNum = 7
@@ -29,6 +32,7 @@
 //console.log(word.length);
 //console.log(word.charAt(3));
 
+
 //Conditionals
 //var item = 300
 //var budget = 100
@@ -38,12 +42,14 @@
 //   console.log("Within budget!")
 //}
 
+
 // var hungry = true
 // if(hungry === true){
 //     console.log("go eat food")
 // } else {
 //     console.log("go ahead and keep coding")
 // }
+
 
 // var trafficLight = "green"
 // trafficLight = "yellow"
@@ -57,6 +63,7 @@
 //     console.log("STOP!")
 // }
 
+
 // var num1 = 91
 // var num2 = 9
 
@@ -68,11 +75,12 @@
 //     console.log("Error")
 // }
 
-var num = 0
-if(num % 2 === 0){
-    console.log("even")
-} else if(num == 0){
+
+var num = 1
+if(num === 0){
     console.log("zero")
+} else if(num % 2 === 0){
+    console.log("even")
 } else {
     console.log("odd")
 }
