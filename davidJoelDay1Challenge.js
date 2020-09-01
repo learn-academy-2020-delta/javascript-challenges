c21579d218227784f5175c26f90b8d7ce30a7227
davidkim4 <dkim4016@gmail.com>
2020-09-01 13:44:59 -0700

    arithmetic and string added

--- davidJoelDay1Challenge.js
+++ davidJoelDay1Challenge.js
@@ -1,4 +1,14 @@
 // David & Joel
 
+//Arithmetic
+//console.log(34 + 71); 
+//console.log(123 - 67);
+//console.log(56 * 23);
+//console.log(45 / 5);
+//console.log(5 ** 7);
+//console.log(33 % 6);
 
+//Strings
+//console.log("David".length);
+//console.log("Joel".charAt(2));
 
