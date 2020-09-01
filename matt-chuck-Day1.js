996bbc687145eb2294545cfb35301c9ff2de0469
mmullis07 <eddiemull311@gmail.com>
2020-09-01 15:05:55 -0700

    Added Boolean, and half of Variable challenges

--- matt-chuck-Day1.js
+++ matt-chuck-Day1.js
@@ -1,3 +1,4 @@
+//Arithmetic Challenges
 console.log(34 + 71);
 console.log(123 - 67);
 console.log(56 * 23);
@@ -5,6 +6,21 @@ console.log(45 / 5);
 console.log(5 ** 7);
 console.log(33 % 6);
 
+//String Challenges
 var name = "ChuckMatt"
 console.log(name.length);
 console.log(name.includes("e"));
+
+//Boolean Challenges
+console.log(34 / 3 > 67 / 2);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(!3 === 3);
+console.log("LEARN".length === 5 && "Student".length === 7);
+console.log("LEARN".length === 5 || "Student".length === 10);
+
+//Variable Challenges
+var favNum = 7;
+var otherNum = 13;
+console.log(favNum / 2);
+console.log(favNum / otherNum);
