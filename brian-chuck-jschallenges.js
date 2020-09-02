d6345dcb7d5a9d28229d0ff38a6b71e8d4361a93
crgjr66 <chuckgilbreth@yahoo.com>
2020-09-02 15:56:49 -0700

    first 5 FLA complete

--- brian-chuck-jschallenges.js
+++ brian-chuck-jschallenges.js
@@ -147,27 +147,102 @@
 // console.log(greaterNum(4, 1));
 // console.log(greaterNum(23, 23));
 
-//Write a function helloWorld, takes a language code
-const helloWorld = (langCode) => {
-//verify language code is a two letter valid myString
-  if (typeof langCode === "string" && langCode.length === 2) {
-// Verify language is supported, it = Italian, sp = Spanish, fr = French
-      if (langCode === "it") {
-        return "Ciao mondo!"
-      } else if (langCode === "sp") {
-        return "Hola mundo!"
-      } else if (langCode === "fr") {
-        return "Bonjour le monde!"
-      } else {
-        return "Hello world!"
-      }
-  } else {
-    return "Invalid language code."
-  }
-}
+// //Write a function helloWorld, takes a language code
+// const helloWorld = (langCode) => {
+// //verify language code is a two letter valid myString
+//   if (typeof langCode === "string" && langCode.length === 2) {
+// // Verify language is supported, it = Italian, sp = Spanish, fr = French
+//       if (langCode === "it") {
+//         return "Ciao mondo!"
+//       } else if (langCode === "sp") {
+//         return "Hola mundo!"
+//       } else if (langCode === "fr") {
+//         return "Bonjour le monde!"
+//       } else {
+//         return "Hello world!"
+//       }
+//   } else {
+//     return "Invalid language code."
+//   }
+// }
+//
+// console.log(helloWorld("it"));
+// console.log(helloWorld("sp"));
+// console.log(helloWorld("fr"));
+// console.log(helloWorld("en"));
+// console.log(helloWorld("zmb"));
 
-console.log(helloWorld("it"));
-console.log(helloWorld("sp"));
-console.log(helloWorld("fr"));
-console.log(helloWorld("en"));
-console.log(helloWorld("zmb"));
+// var testArr1 = [3, 9, 15, 4, 10];
+// // write a function that takes in an array
+// const multiplyByThree = (array) => {
+//   var newArray = [];
+//   // loop through the array
+//   for (let i = 0; i < array.length; i++) {
+//     // multiply all number in array by 3
+//     newArray[i] = array[i] * 3;
+//   }
+//   // return an array of the new numbers
+//   return newArray;
+// }
+//
+// console.log(multiplyByThree(testArr1));
+
+
+// // return a new array with those odd Numbers
+// var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13];
+// // write a funtion that takes an array
+// const findOdds = (array) => {
+//   var oddArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     // identifiy any odd numbers in the array
+//     if ((array[i] % 2 === 1) || (array[i] % 2 === -1)) {
+//       oddArray.push(array[i]);
+//     }
+//   }
+//   return oddArray;
+// }
+//
+// console.log(findOdds(testArr2));
+
+// write a function that takes a string
+// capitalize every letter in the string
+// return as a new string
+// var myMessage = "Hello There";
+//
+// const capMessage = (message) => {
+//   return message.toUpperCase();
+// }
+//
+// console.log(capMessage(myMessage));
+
+
+// var comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"];
+// // write a function that takes numbers and letters and booleans
+// const getLetters = (array) => {
+//   var newString = "";
+//   for (let i = 0; i < array.length; i++) {
+//     // seperate out letters
+//     if (typeof array[i] === "string") {
+//       newString = newString + array[i];
+//     }
+//   }
+//   // return a string of those letters
+//   return newString;
+// }
+// console.log(getLetters(comboArr));
+
+// var highestNumber = [1, 45, 4, 2, 7, 67, -9, 0, 6];
+// // write a function that takes an array
+// const bigNumber = (array) => {
+//   var biggestNumber = array[0];
+//   for (let i = 0; i < array.length; i++) {
+//     // identify the highest number in the array
+//     if (array[i] > biggestNumber) {
+//       biggestNumber = array[i];
+//     }
+//   }
+//   // return the number
+//   return biggestNumber;
+// }
+//
+// console.log(bigNumber(highestNumber));
