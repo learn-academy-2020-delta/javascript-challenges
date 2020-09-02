2be276fa89df8a798d09a5ce748d7ec4348bbf1a
DiemBTran <diembtran@gmail.com>
2020-09-02 10:26:29 -0700

    second loop challenge

--- loops-joel-diem.js
+++ loops-joel-diem.js
@@ -1,17 +1,40 @@
+// Logging values with for loops
+// for(let i = 0; i < 20; i++) {
+//     console.log(i+1);
+// }
+//
+// for(let i = 0; i < 20; i++) {
+//     console.log((i+1) * 3);
+// }
+//
+// for(let i = 0; i < 20; i++) {
+//     if((i+1) % 2 === 0) {
+//         console.log(i+1);
+//     } else {
+//         console.log("ODD")
+//     }
+// }
 
-for(let i = 0; i < 20; i++) {
-    console.log(i+1);
+// Looping over an array
+var nums = [3, 57, -9, 20, 67];
+var biggerNum = 0;
+for (let i = 0; i < nums.length; i++)
+{
+  if (nums[i] > biggerNum){
+    biggerNum = nums[i];
+  }
 }
+console.log(biggerNum);
 
-for(let i = 0; i < 20; i++) {
-    console.log((i+1) * 3);
+for (let i = 0; i < nums.length; i++)
+{
+  if (nums[i] < biggerNum){
+    biggerNum = nums[i];
+  }
 }
+console.log(biggerNum);
 
-for(let i = 0; i < 20; i++) {
-    if((i+1) % 2 === 0) {
-        console.log(i+1);
-    } else {
-        console.log("ODD")
-    }
+for (let i = 0; i < nums.length; i++)
+{
+  console.log(nums[i] % 2);
 }
-
