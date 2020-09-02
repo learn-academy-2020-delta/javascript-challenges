5e5de95e9ece37ed342f0b09c9780248d297bcac
crgjr66 <chuckgilbreth@yahoo.com>
2020-09-02 10:00:48 -0700

    finished 1st exercise

---
+++ brian-chuck-jschallenges.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 20; i++) {
+  console.log(i * 3);
+}
+
+for (var i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  } else {
+    console.log("ODD");
+  }
+}
