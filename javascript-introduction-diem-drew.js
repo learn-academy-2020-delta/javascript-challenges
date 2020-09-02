011253f7ab071a2057afe193c422eff7e114f737
drewbrad4 <drewbrad4@gmail.com>
2020-09-01 17:42:23 -0700

    Final Commit

--- javascript-introduction-diem-drew.js
+++ javascript-introduction-diem-drew.js
@@ -1,110 +1,110 @@
 // Arithmetic
-console.log("Arthimetic answers");
+console.log("Arthimetic Answers");
 
-console.log(34 + 71);
-console.log(123 - 67);
-console.log(56 * 23);
-console.log(45 / 5);
-console.log(5 ** 7);
-console.log(33 % 6);
+console.log("34 + 71:", 34 + 71);
+console.log("123 - 67:", 123 - 67);
+console.log("56 * 23:", 56 * 23);
+console.log("45 / 5:", 45 / 5);
+console.log("5 ** 7:", 5 ** 7);
+console.log("33 % 6:", 33 % 6);
 
 // String
-console.log("String answers");
+console.log("\nString Answers");
 var name = "Diem";
 
-console.log(name.length);
-console.log(name.includes("e"));
+console.log("String Length:", name.length);
+console.log("Includes 'e':", name.includes("e"));
 
 // Boolean
-console.log("Boolean answers");
-console.log(34/3 > 67/2);
-console.log(5 == "5");
-console.log(5 === "5");
-console.log(!3 === 3);
-console.log("LEARN".length === 5 && "Student".length === 7);
-console.log("LEARN".length === 5 || "Student".length === 10);
+console.log("\nBoolean Answers");
+console.log("34/3 > 67/2:", 34/3 > 67/2);
+console.log("5 == '5':", 5 == "5");
+console.log("5 === '5':", 5 === "5");
+console.log("!3 === 3:", !3 === 3);
+console.log("'LEARN'.length === 5 && 'Student'.length === 7:", "LEARN".length === 5 && "Student".length === 7);
+console.log("'LEARN'.length === 5 || 'Student'.length === 10:", "LEARN".length === 5 || "Student".length === 10);
 
 // Variables
-console.log("Variable answers");
+console.log("\nVariable Answers");
 
 var favNum = 5;
-console.log(favNum);
-console.log(favNum / 2);
+console.log("favNum:", favNum);
+console.log("favNum / 2:", favNum / 2);
 
 var otherNum = 13;
-console.log(favNum / otherNum);
+console.log("favNum / otherNum", favNum / otherNum);
 
 var string = "Heyyy There!";
-console.log(string.length);
-console.log(string.charAt(3));
+console.log("String Length:", string.length);
+console.log("CharAt[3]:", string.charAt(3));
 
 // Conditional Challenges
-console.log("Conditional Challenges Answers")
+console.log("\nConditional Challenge Answers")
 var items = 0, price = 0;
 if (price <= 100){
-  console.log("in budget");
+  console.log("Budget Test:", "in budget");
 } else {
-  console.log("out of budget");
+  console.log("Budget Test:", "out of budget");
 }
 
 var hungry = true;
 if (hungry === true){
-  console.log("Eat food");
+  console.log("Hunger Test:", "Eat food");
 } else {
-  console.log("Keep coding");
+  console.log("Hunger Test:", "Keep coding");
 }
 
 var trafficLight = "yellow";
 if (trafficLight === "green"){
-  console.log("Go!");
+  console.log("Traffic Light:", "Go!");
 } else if (trafficLight === "yellow"){
-  console.log("Slow down!");
+  console.log("Traffic Light:", "Slow down!");
 } else if (trafficLight === "red"){
-  console.log("Stop!");
+  console.log("Traffic Light:", "Stop!");
 } else{
   console.log("Error!");
 }
 
 var num1 = 0, num2 = 1;
 if (num1 > num2){
-  console.log(num1);
+  console.log("Biggest Num:", num1);
 } else if (num1 < num2){
-  console.log(num2);
+  console.log("Biggest Num:", num2);
 } else if (num1 === num2){
-  console.log("They're equal")
+  console.log("Biggest Num:", "They're equal")
 } else{
   console.log("You did it wrong! Redo it!");
 }
 
 var number = 27;
 if (number === 0){
-  console.log("It's zero");
+  console.log("Even or Odd:", "It's zero");
 } else if (number%2 === 1){
-  console.log("It's odd");
+  console.log("Even or Odd:", "It's odd");
 } else if (number%2 === 0){
-  console.log("It's even");
+  console.log("Even or Odd:", "It's even");
 } else{
   console.log("Input error, please enter a number");
 }
 
 // Stretch Challenges
-console.log("Stretch Challenge answers");
+console.log("\nStretch Challenge Answers");
 
 var gradePercentage = 40
 if(gradePercentage === 100){
-  console.log("Perfect Score!!");
+  console.log("Test Grade:", "Perfect Score!!");
 } else if(gradePercentage === 0){
-  console.log("No grade available");
+  console.log("Test Grade:", "No grade available");
 } else if(gradePercentage <= 99 && gradePercentage >= 90){
-  console.log("You got an A!");
+  console.log("Test Grade:", "You got an A!");
 } else if(gradePercentage <= 89 && gradePercentage >= 80){
-  console.log("You got an B!");
+  console.log("Test Grade:", "You got an B!");
 } else if(gradePercentage <= 79 && gradePercentage >= 70){
-  console.log("You got an C!");
+  console.log("Test Grade:", "You got an C!");
 } else if(gradePercentage <= 69 && gradePercentage >= 60){
-  console.log("You got an D!");
+  console.log("Test Grade:", "You got an D!");
 } else if(gradePercentage <= 59){
-  console.log("You got an F!");
+  console.log("Test Grade:", "You got an F!");
 } else {
   console.log("Input Error");
 }
@@ -112,60 +112,65 @@ if(gradePercentage === 100){
 
 var data = true;
 if(typeof data === "string"){
-  console.log("string");
+  console.log("Data Type:", "string");
 } else if(typeof data === "boolean"){
-  console.log("boolean");
+  console.log("Data Type:", "boolean");
 } else if(typeof data === "number"){
-  console.log("number");
+  console.log("Data Type:", "number");
 } else {
   console.log("Input Error");
 }
 
 var password = "heavypineapple"
 if(password.length >= 12 && password.includes("!")){
-  console.log("This is a mighty strong password");
+  console.log("Password Strength:", "This is a mighty strong password");
 } else if(password.length >= 8 || password.includes("!")){
-  console.log("This password is strong enough");
+  console.log("Password Strength:", "This password is strong enough");
 } else {
   console.log("That is not a VALID password");
 }
 
 // Array Challenges
+console.log("\nArray Challenge Answers")
+console.log("groceryList = ['chips', 'dip', 'cookies']")
 var groceryList = ["chips", "dip", "cookies"];
 groceryList.push("soda");
-console.log(groceryList);
+console.log("Add [soda]:", groceryList);
 
 var groceryList2 = ["granola"];
-console.log("Original array", groceryList);
-console.log("New array", groceryList.concat(groceryList2));
+console.log("Add [granola]:", "Original array:", groceryList);
+console.log("Add [granola]:", "New array:", groceryList.concat(groceryList2));
 
+console.log("\nnumbers = [2, 4, 6, 8, 10]")
 var numbers = [2, 4, 6, 8, 10];
 numbers.unshift(0);
-console.log(numbers);
+console.log("Add [0] to beginning:", numbers);
 numbers.push(12);
-console.log(numbers);
+console.log("Add [12] to end", numbers);
 numbers.shift();
-console.log(numbers);
+console.log("Remove first number:", numbers);
 var numbers2 = [0];
-console.log("Original array", numbers);
-console.log("New array", numbers.concat(numbers2));
+console.log("Add [0]:", "Original array:", numbers);
+console.log("Add [0]:", numbers.concat(numbers2));
 
+console.log("\nnumSet = [2, 13, 6, 8, 4, 2]")
 var numSet = [2, 13, 6, 8, 4, 2];
-console.log(numSet.indexOf(2));
-console.log(numSet.lastIndexOf(2));
+console.log("Index of 2:", numSet.indexOf(2));
+console.log("Last Index of 2:", numSet.lastIndexOf(2));
 
+console.log("\nchars = ['y', 'a', 'r', 'r', 'a']")
 var chars = ["y", "a", "r", "r", "a"]
-console.log(chars.join(""));
-console.log(chars.reverse());
-console.log(chars.join("-"));
-console.log(chars.join(""));
+console.log("Join Array:", chars.join(""));
+console.log("Reverse Array:", chars.reverse());
+console.log("Join with -:", chars.join("-"));
+console.log("Join:", chars.join(""));
 
+console.log("\ndelta = ['Drew', 'Diem', 'Brian']")
+console.log("delta2 = ['Bach', 'Nathan', 'Joel']")
 var delta = ["Drew", "Diem", "Brian"];
 var delta2 = ["Bach", "Nathan", "Joel"];
 
-console.log(delta.sort());
-console.log(delta.reverse());
-// delta.sort(delta.concat(delta2));
-// console.log(delta);
+console.log("Sort:", delta.sort());
+console.log("Reverse:", delta.reverse());
 var delta3 = delta.concat(delta2);
-console.log(delta3.sort());
+console.log("Concat and Sort:", delta3.sort());
