31aa6fea7797afed93380d663d1abb17739cb8b9
ryanizumi16 <ryanizumi16@gmail.com>
2020-09-02 10:37:15 -0700

    complet

--- javascript-loops-ryan-drew.js
+++ javascript-loops-ryan-drew.js
@@ -38,8 +38,49 @@ for(let i = 0; i < nums.length; i++){
 }
 
 // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
-
+for(let i = 0; i < nums.length; i++) {
+  //console.log(nums[i] % 2 )
+}
 // Looping over a string. Consider this variable:
-// var myString = "learn student"
-
 // Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
+var myString = "learn student"
+var count = 0
+
+for(let i = 0; i < myString.length; i++) {
+  if(myString[i].includes("e")){
+    count++
+}
+}
+//console.log(count)
+
+//Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
+
+for(let i = 0; i < 16; i++) {
+  if(i % 2 === 0){
+    //console.log(`${i} is even`)
+  }
+  else if(i % 2 !== 0){
+    //console.log(`${i} is odd`)
+  }
+}
+
+
+
+
+
+//Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+
+for(let i = 1; i <= 100; i++) {
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz")
+  }
+  else if(i % 3 === 0){
+    console.log("fizz")
+  }
+  else if(i % 5 === 0){
+    console.log("buzz")
+  }
+  else{
+    console.log(i)
+  }
+  }
