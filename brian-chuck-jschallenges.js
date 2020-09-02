bae3ee97b4432a0c1ee23b238a9ba5f12457dbd8
brianthomashammond <brianthomashammond@gmail.com>
2020-09-02 10:26:00 -0700

    final changes

--- brian-chuck-jschallenges.js
+++ brian-chuck-jschallenges.js
@@ -1,15 +1,38 @@
-for (let i = 1; i <= 20; i++) {
-  console.log(i);
-}
-
-for (let i = 1; i <= 20; i++) {
-  console.log(i * 3);
-}
-
-for (var i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  } else {
-    console.log("ODD");
-  }
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+//
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i * 3);
+// }
+//
+// for (var i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } else {
+//     console.log("ODD");
+//   }
+// }
+// var nums = [3, 57, -9, 20, 67]
+// var highNum = 0
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] > highNum) {
+//     highNum = nums[i]
+//   }
+// }
+//
+// console.log(highNum);
+//
+// var nums = [3, 57, -9, 20, 67]
+// var lowNum = 0
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] < lowNum) {
+//     lowNum = nums[i]
+//   }
+// }
+//
+// console.log(lowNum);
+var nums = [3, 57, -9, 20, 67]
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums[i] % 2);
 }
