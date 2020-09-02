5a8469e19d92e2e8b52955648d947dc678147441
brianthomashammond <brianthomashammond@gmail.com>
2020-09-02 10:42:05 -0700

    all stretched out

--- brian-chuck-jschallenges.js
+++ brian-chuck-jschallenges.js
@@ -44,12 +44,24 @@
 //     count++;
 //   }
 // }
-// console.log(count);
-
-for (let i = 0; i <= 15; i++) {
-  if (i % 2 === 0) {
-    console.log(i + " is even!");
-  } else {
-    console.log(i + " is odd!");
-  }
-}
+// // console.log(count);
+//
+// for (let i = 0; i <= 15; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + " is even!");
+//   } else {
+//     console.log(i + " is odd!");
+//   }
+// }
+//
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("fizzbuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }
