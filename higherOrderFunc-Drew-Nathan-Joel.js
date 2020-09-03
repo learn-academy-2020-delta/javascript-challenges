3941f296c3922291a734f774fce9c05363eea933
NateDawg360 <nathan_loranca@yahoo.com>
2020-09-03 11:00:40 -0700

    Updated challenges

--- higherOrderFunc-Drew-Nathan-Joel.js
+++ higherOrderFunc-Drew-Nathan-Joel.js
@@ -19,4 +19,38 @@ const oddNum = (arr) => {
 // Expected output --> [7, 3, 5, 13]
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
-// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"] Expected output --> "nicework"
+
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const onlyLetters = (arr) => {
+let newString = arr.filter (value => typeof value === "string")
+return newString.join("")
+}
+console.log(onlyLetters(comboArr));
+
+
+
+// Create a function that takes in a string and returns a new string with all the vowels removed.
+
+var str = "javascript is awesome"
+const noVowels = (string) => {
+let newArray = string.split("")
+let filteredArray = newArray.filter(value => {
+return value !== "a" && value !== "e" && value !=="i" && value !=="o" && value !=="u" && value !== "y"
+})
+let newString = filteredArray.join("")
+  return newString
+}
+console.log(noVowels(str));
+//
+// //
+// Expected output --> "jvscrpt s wsm"
+//
+
+
+
+// Create a function that takes in two arrays as arguments returns one array with no duplicate values.
+// var arr1 = [3, 7, 10, 5, 4, 3]
+//
+// var arr2 = [7, 8, 2, 1, 5, 4]
+//
+// Expected output -> [3, 7, 10, 5, 4, 8, 2, 1]
