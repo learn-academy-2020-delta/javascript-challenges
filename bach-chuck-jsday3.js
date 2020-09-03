e98b0658a245e8af743bc2581b1cec16e7705c5b
crgjr66 <chuckgilbreth@yahoo.com>
2020-09-03 10:02:04 -0700

    added first 2 challenge exercises

---
+++ bach-chuck-jsday3.js
@@ -0,0 +1,21 @@
+// var arr1 = [3, 9, 15, 4, 10];
+// // write a function that takes in an array
+// const mult10 = (array) => {
+//   // multiply all numbers by 10
+//   // return the new array
+//   return array.map(value => value * 10)
+// }
+//
+// console.log(mult10(arr1));
+
+// var arr2 = [2, 7, 3, 5, 8, 10, 13];
+// // Write a function that takes in an array
+// const findOdd = (array) => {
+//   // find only odd numbers.
+//   // return the new array
+//   return array.filter(value => {
+//     return (value % 2 === 1)
+//   })
+// }
+//
+// console.log(findOdd(arr2));
