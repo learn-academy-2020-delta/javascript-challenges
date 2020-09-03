c51b3198c089cc8212f727f0c462d827fe725d14
crgjr66 <chuckgilbreth@yahoo.com>
2020-09-03 14:15:05 -0700

    adding initial version of testing folder

---
+++ bach-chuck-test/myApp.test.js
@@ -0,0 +1,37 @@
+// Write the test for a function that returns "drink coffee" if you are
+// tired and "keep working" if you are not tired. Write the function that // will make the test pass
+// describe ("needCoffee", () => {
+//   test("take an argument and determine if the person needs coffee", () => {
+//     expect(needCoffee("tired")).toEqual("drink coffee")
+//     expect(needCoffee("not tired")).toEqual("keep working")
+//   })
+// })
+//
+// const needCoffee = (condition) => {
+//   if (condition === "tired") {
+//     return "drink coffee"
+//   } else if (condition === "not tired") {
+//     return "keep working"
+//   } else {
+//     return "incorrect input"
+//   }
+// }
+
+// Write the test for a function that returns "relax" if you are stressed // and "keep going" if you are not stressed. Write the function that
+// will make the test pass.
+// describe ("areStressed", () => {
+//    test("take an argument and determine if the person needs rest", () => {
+//      expect(areStressed("stressed")).toEqual("relax")
+//      expect(areStressed("not stressed")).toEqual("keep going")
+//    })
+//  })
+//
+//  const areStressed = (condition) => {
+//    if (condition === "stressed") {
+//      return "relax"
+//    } else if (condition === "not stressed") {
+//      return "keep going"
+//    } else {
+//      return "incorrect input"
+//    }
+//  }
