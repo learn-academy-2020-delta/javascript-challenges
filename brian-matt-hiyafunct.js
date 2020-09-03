b4e22a5d435cb92e3642a3c6d3e50eba480a243f
mmullis07 <eddiemull311@gmail.com>
2020-09-03 10:11:30 -0700

    Completed 1st challenges

---
+++ brian-matt-hiyafunct.js
@@ -0,0 +1,31 @@
+//Write a function that takes in an array and returns a new array with all numbers multiplied by 10.
+/*var arr1 = [3, 9, 15, 4, 10]
+//Declare a function that cycles through the array
+const times10 = (arr) => {
+// return the values of the array multiplied by 10
+    return arr.map(value => value * 10)
+};
+console.log(times10(arr1)); */
+
+//Write a function that takes in an array and returns a new array with only odd numbers.
+/*var arr2 = [2, 7, 3, 5, 8, 10, 13]
+//Declare a function that cycles through the array
+const oddNums = (arr) => {
+    //Identify and Return the values that are odd
+    return arr.filter(value => value % 2 === 1)
+};
+console.log(oddNums(arr2)); */
+
+//Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
+/*var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"] 
+//Declare a function that will cycle through the array
+const letterString = (arr) => {
+    //Extract letters only
+    return arr.filter(value => typeof value === "string").join("")
+    }
+    //return a string of the letters in the new array
+    console.log(letterString(comboArr));*/
+
+
+
+
