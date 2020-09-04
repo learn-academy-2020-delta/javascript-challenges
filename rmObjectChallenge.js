cb00a66eb4c37ee16f0b748b5e1157fd6680d83f
mmullis07 <eddiemull311@gmail.com>
2020-09-04 11:31:11 -0700

    half of object challenges

---
+++ rmObjectChallenge.js
@@ -0,0 +1,33 @@
+// var person = {
+//     firstName: "Arthur",
+//     lastName: "Dent",
+//     getData: function() {
+//         return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+//     }
+//     }
+//     person.homePlanet = "Earth"
+    
+// //console.log(person.firstName)  
+// //console.log(person.lastName)
+// //console.log(person.homePlanet)
+// console.log(person.getData())
+
+// var product = {
+//     name: "chair", price: 14.99,
+//     describeProduct: function (product) {
+//         return `The product is a ${this.name}. It costs $${this.price}.`
+//     }
+//   }
+//   console.log(product.describeProduct());
+
+var lunch = {
+    name: "PB and Banana",
+    type: "sandwich",
+    ingredients: ["bread", "peanut butter", "banana"],
+    sandIngredients: function (lunch) {
+        return `The ingredients for ${this.name} ${this.type} are ${this.ingredients}.`
+    }
+  }
+  //console.log(lunch.ingredients)
+  //console.log(lunch.ingredients[2])
+  console.log(lunch.sandIngredients())
