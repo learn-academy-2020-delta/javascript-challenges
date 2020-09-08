d682becf2d6cf249b3d798f735d1fa4c74faf4e4
davidkim4 <dkim4016@gmail.com>
2020-09-08 13:30:57 -0700

    rest of class challenges

--- brian-david-classes.js
+++ brian-david-classes.js
@@ -25,20 +25,49 @@
 //     }
 //   }
 // }
-//
+
+
+//#1
 // var blackCoffee = new Coffee("coffee", 0, 0);
 // var normalCoffee = new Coffee("coffee", 1, 2);
 // var secondCoffee = new Coffee("coffee", 0, 2);
 // console.log(secondCoffee.coffeeProfile());
 
-class Latte {
-  constructor(milk, milkType, numberOfShots) {
-    this.flavor = flavor,
-    this.milkType = milkType,
-    this.numberOfShots = numberOfShots,
-  }
 
-  latteProfile() {
-    return `flavor: ${flavor}, milk type: ${milkType}, number of shots: ${numberOfShots}`
-  }
-}
+//#2
+// class Latte {
+//   constructor(flavor, milkType, numberOfShots) {
+//     this.flavor = flavor,
+//     this.milkType = milkType,
+//     this.numberOfShots = numberOfShots
+//   }
+
+//   latteProfile() {
+//     return `flavor: ${flavor}, milk type: ${milkType}, number of shots: ${numberOfShots}`
+//   }
+// }
+
+// var singleRegular = new Latte("regular", "milk", "1")
+// console.log(singleRegular);
+
+// var fancyDrink = new Latte("hazelnut", "almond", "2")
+// console.log(fancyDrink);
+
+
+//#3
+// class Cylinder{
+//   constructor(radius, height){
+//     this.radius = radius,
+//     this.height = height
+//   }
+
+//   cylinderVolume(){
+//     return Math.PI*this.radius**2*this.height
+//   }
+// }
+// var cylinder1 = new Cylinder(1,1)
+// var cylinder2 = new Cylinder(2,1)
+// var cylinder3 = new Cylinder(3,7)
+// console.log(cylinder1.cylinderVolume());
+// console.log(cylinder2.cylinderVolume());
+// console.log(cylinder3.cylinderVolume());
